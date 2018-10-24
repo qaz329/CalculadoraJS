d00d17007c786dfd723274199f7a76c04fe11978
Joan Espuñes <joanespunyes@hotmail.com>
2018-10-24 19:06:19 +0200

    Funcionament casi correcte de la calculadora

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 (function(){
+    // Resultat
+    var resultat;
+    // Resultat anterior
+    var resAnterior;
+    // Historial
+    var historial;
     // Numero actual
     var numActual;
     // Boto clicat abans
@@ -7,6 +13,12 @@
     var clicaOperador;
     // Operador clicat
     var operador;
+    // Operador clicat anteriormentcalcc
+    var operadorAnterior;
+
+    var contadorOperadors;
+    // Reset?
+    var esReset;
     // Pantalla principal
     var pantallaPrincipal = document.querySelector('#principal');
     // Pantalla historial
@@ -22,60 +34,239 @@
   	}
 
     function reset(){
-
+      resultat = null;
+      numActual = 0;
+      btnAnterior = null;
+      operador = null;
+      operadorAnterior = null;
+      contadorOperadors = 0;
+      historial = '';
+      clicaOperador = false;
+      esReset = true;
+      actualitzaPantalla(0);
+      actualitzaHistorial(historial);
     }
 
     function input(btn){
-      /* TODO
+
+      if (!isNaN(btnAnterior) && btn !== '=' && btn !== 'C' && btn !== 'CE' && btn !== 'CS' && btn !== '.') {
+          operadorAnterior = operador;
+      }
+
       switch(btn) {
           case '+':
             clicaOperador = true;
-            operador = addition;
+            operador = suma;
             break;
           case '-':
             clicaOperador = true;
-            operador = subtraction;
+            operador = resta;
             break;
           case '/':
             clicaOperador = true;
-            operador = division;
+            operador = divisio;
             break;
           case '*':
             clicaOperador = true;
-            operador = multiplication;
+            operador = multiplicacio;
             break;
           case 'C':
             reset();
             break;
-      }*/
+      }
 
       gestionaBoto(btn);
+
+      var fontSize = parseFloat(pantallaPrincipal.style.fontSize);
+      // return to default main screen size
+      if (fontSize < 3 && numActual.length < 11) {
+          pantallaPrincipal.style.fontSize = '3rem';
+      }
+
+      console.log('Result: ' + resultat);
+      console.log('Prev result: ' + resAnterior);
+      console.log('current number: ' + numActual);
+      console.log('btn: ' + btn);
+      console.log('Prev Math Op: ' + operadorAnterior);
+      console.log('Math Op Counter: ' + contadorOperadors);
+      console.log('Prev btn: '+ btnAnterior);
+      console.log('History: ' + historial);
+      console.log('Main Screen ' + pantallaPrincipal.value);
+      console.log('*'.repeat(15));
     }
 
     function gestionaBoto(btn){
+      // return si C no es clica quan es divideix per 0
+      if (btn !== 'C' && resultat === 'Result is undefined' || resultat === 'Cannot divide by zero') {
+          return;
+      }
+
+      // actualitza historial
+      if (btn !== '=' && btn !== 'C' && btn !== 'CE' && btn !== 'CS') {
+          historial = (isNaN(btnAnterior) && isNaN(btn)) ? historial.slice(0, -1) + btn : historial + btn;
+      }
+
       // Si el boto es un Numero o '.'
       if (!isNaN(btn) || btn === '.') {
           // Expressió regular que comprova si 'numActual' es un numero
           if (btn === '.' && /^\d+$/.test(numActual)) {
               numActual = numActual + btn;
           } else if (!isNaN(btn)) {
-              if((!isNaN(btnAnterior) && btnAnterior !== null && pantallaPrincipal.value !== '0') || btnAnterior === '.'){
-                numActual = numActual + btn;
-              }else{
-                numActual = btn;
-              }
+              numActual = (!isNaN(btnAnterior) && btnAnterior !== null && pantallaPrincipal.value !== '0') || btnAnterior === '.' ? numActual + btn : btn;
           }
           clicaOperador = false;
           actualitzaPantalla(numActual);
-      }
+      }else{
+        // actualitza historial
+        if (btn === '-' || btn === '+' || btn === '*' || btn === '/') {
+            // per exemple, quan es cliqui `-`, afegir `0 -` al historial
+            if ((btnAnterior === null || btnAnterior === '=') && !esReset) {
+                historial = '0' + btn;
+                contadorOperadors++;
+            }
+
+            if (!pantallaHistorial.value.length && pantallaPrincipal.value.length) {
+                historial = pantallaPrincipal.value + btn;
+            }
+          }
+
+            // if math op was pressed and result is null
+            if (operador && resultat === null) {
+                resultat = Number(numActual);
+            }
+
+            // percentatges
+            if (btn === '%') {
+                historial = historial.slice(0, -(numActual.length + 1));
+                numActual = percentatge(numActual, resultat);
+                historial += numActual + ' ';
+                actualitzaPantalla(numActual);
+            // arrel
+            } else if (btn === 'quadrat' || btn === 'arrel' || btn === '1/x') {
+                historial = historial.slice(0, -(numActual.length + btn.length)) + (btn === '1/x' ? '1/(' + numActual + ') ' : btn + '(' + numActual + ') ');
+
+                if(btn === 'quadrat'){
+                  numActual = quadrat(numActual);
+                }else if(btn === 'arrel'){
+                  numActual = arrel(numActual);
+                }else{
+                  numActual = fraccio(numActual);
+                }
+
+                actualitzaPantalla(numActual);
+                actualitzaHistorial(historial);
+            }
+
+            // resultat
+            if (btn === '=') {
+                // si existeix operador
+                if (operador) {
+                    contadorOperadors = 0;
+                    // si el ultim boto abans de '=' es un operador
+                    if (clicaOperador) {
+                        operador(resAnterior);
+                    // if last button pressed is `digit` before `=` was pressed
+                    } else {
+                        operador(Number(numActual));
+                    }
+
+                    historial = '';
+                    btnAnterior = btn;
+
+                    actualitzaPantalla(resultat);
+                    actualitzaHistorial(historial);
+
+                    return;
+                }
+            }
+
+            // contar operadors
+
+            if (isNaN(btn) && (!isNaN(btnAnterior) || btnAnterior === '%' || btnAnterior === 'sqr' || btnAnterior === 'sqrt' || btnAnterior === '1/x') &&
+                btn !== '=' && btn !== 'C' && btn !== 'CE' && btn !== 'CS' && btn !== '.' && btn !== '%' && btn !== 'sqr' & btn !== 'sqrt' && btn !== '1/x') {
+                contadorOperadors++;
+            }
+
+
+            if (contadorOperadors >= 2 && (!isNaN(btnAnterior) || btnAnterior === 'sqrt' || btnAnterior === 'sqr' || btnAnterior === '1/x' || btnAnterior === '%') && btn !== 'CE' && btn !== 'CS') {
+                operadorAnterior(Number(numActual));
+                actualitzaPantalla(resultat);
+            }
+
+            if (btn === 'CE' && historial.length > 0) {
+                historial = historial.slice(0, -(numActual.length));
+                numActual = '0';
+                actualitzaPantalla(0);
+            } else if (btn === 'CS') {
+                if (resultat != pantallaPrincipal.value) {
+                    numActual = numActual.slice(0, -1);
+                    historial = historial.slice(0, -1);
+                    if (!numActual.length) {
+                        numActual = '0';
+                    }
+                    actualitzaPantalla(numActual);
+                } else {
+                    return;
+                }
+            }
+
+            if (resultat !== null && btn !== 'CE' && btn !== 'CS') {
+                actualitzaHistorial(historial);
+            }
+          }
+
+      btnAnterior = btn;
+      resAnterior = resultat;
+      esReset = false;
     }
 
     function actualitzaPantalla(val) {
 
         val = String(val);
 
-        //TODO comprovar quants nums hi ha en pantalla per fer-los mes petits
+        if (val.length > 10) {
+            pantallaPrincipal.style.fontSize = '1.75rem';
+            val = Math.round(val * 10000000000000000) / 10000000000000000;
+        }
 
         pantallaPrincipal.value = val;
     }
+
+    function actualitzaHistorial(historial) {
+        pantallaHistorial.value = historial;
+    }
+
+    function suma(val) {
+        resultat += val;
+    }
+
+    function resta(val) {
+        resultat -= val;
+    }
+
+    function divisio(val) {
+        resultat /= val;
+    }
+
+    function multiplicacio(val) {
+        resultat *= val;
+    }
+
+    function quadrat(val) {
+        return val * val;
+    }
+
+    function arrel(val) {
+        return Math.sqrt(val);
+    }
+
+    function percentatge(val, res) {
+        return res * val / 100;
+    }
+
+    function fraccio(val) {
+        return 1 / val;
+    }
+
+    reset();
 })();
